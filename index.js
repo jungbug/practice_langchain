fc4b9cdeb1fab1c09efd56034ee21749b08f85be
JUNG SUNG YOON <xbox1111@naver.com>
2023-11-27 21:10:22 +0900

    add env

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import { OpenAI } from "langchain/llms/openai";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 
+import { GPT_API } from "@env"
+
 const llm = new OpenAI({
-  temperature: 0.9,
+  temperature: 0.9, // 낮을수록 보수적인 답변
 });
+
+console.log(GPT_API)
 
 const input = document.querySelector("#input");
 
 const answer = document.querySelector("#answer");
+
+const answer1 = document.querySelector("#answer1");
 
 const chatModel = new ChatOpenAI();
 
@@ -16,3 +22,7 @@ const text = input.ariaValueMax;
 const llmResult = await llm.predict(text);
 
 const chatModelResult = await chatModel.predict(text);
+
+answer.innerHTML = llmResult;
+
+answer1.innerHTML = chatModelResult;
